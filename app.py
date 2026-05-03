b0ae7400cd208c0f5968471026162365f6b2be2b
ninealice-star <ninealice@ninepoints.edu.kg>
2026-05-03 09:24:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ elif st.session_state.step in ["receipt"]:
     </div>
     """, unsafe_allow_html=True)
     st.write("## ")
-    if sn == "1":
+    sn == "1":
   
     else:
         st.balloons(); st.success("All installments verified. Your admission is fully secured.")
